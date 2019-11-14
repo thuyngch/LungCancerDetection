af39f1535be9b5c422d012c945658ba211725e95
AntiAegis <thuypn9a4@gmail.com>
2019-11-14 09:44:30 +0700

    update

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="src",
+    version="0.0.1",
+    author="thuyngch",
+    author_email="thuypn9a4@gmail.com",
+    description="PyTorch Computer Vision",
+    long_description="long_description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/thuyngch/LungCancerDetection",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
