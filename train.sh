ee97acdb1874dbf87db604977525b50fb196ae3c
Thuy Ng <thuypn9a4@gmail.com>
2020-12-29 15:18:56 +0700

    update train script

--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 python src/models/train.py \
     --ckpt "ckpt/attention1.0_softmax_bs8_ep50_trainvaltest/nodule3-classifier.ckpt" \
-    --train_data "src/data/train_val_test.h5" \
-    --valid_data "src/data/test.h5" \
+    --train_data "src/data/train_s100.h5" \
+    --valid_data "src/data/val_s100.h5" \
     --num_outputs 2 \
     --hidden_embedding 512 \
     --lr 1e-4 \
